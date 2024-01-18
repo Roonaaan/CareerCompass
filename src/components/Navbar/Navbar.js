d2d58498a8b55f2110c09a623520a436a9489160
Roonaaan <rjohnronan2001@gmail.com>
2024-01-18 21:34:17 +0800

    Updated Changes - Ramos

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,9 @@
 import React from "react";
 import styles from "./Navbar.module.css";
+import logo from "./final-topright-logo.png"
 
 const Navbar = ({ hamActive, setHamActive }) => {
-  const logo =
-    "https://d2guulkeunn7d8.cloudfront.net/assets/beetstrap/brand/instacart-logo-color-4db9d81ca0b7638befdc4bd331f64a2633df790c0b55ef627c99b1ba77af72b7.svg";
+  const Logo = logo;
   const handleClick = () => {
     setHamActive(!hamActive);
   };
@@ -18,26 +18,15 @@ const Navbar = ({ hamActive, setHamActive }) => {
           <span className={styles.hamburgerLines}></span>
         </button>
         <div className={`${styles.navLeft}`}>
-          <img src={logo} alt="logo" className={styles.brand} />
+          <img src={Logo} alt="logo" className={styles.brand} />
         </div>
         <div className={`${styles.navRight} center`}>
-          <div className={styles.navLinksWrapper}>
-            <div className={styles.verticalLine}> </div>
-            <a href="/" className={`${styles.nav} center`}>
-              Departments
-            </a>
-            <a href="/" className={`${styles.nav} center`}>
-              More ways to shop
-            </a>
-            <a href="/" className={`${styles.nav} center`}>
-              Help
-            </a>
-          </div>
-          <div>
-            <a href="/" className={styles.login}>
+        <a href="/" className={styles.login}>
               Log in
             </a>
             <button className={styles.signup}>Sign Up</button>
+          <div>
+            
           </div>
         </div>
       </div>
