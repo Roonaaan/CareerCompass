fb26ae4b19f22ad17c92a869aaebf150353d710e
Roonaaan <rjohnronan2001@gmail.com>
2024-01-28 16:44:46 +0800

    Updated Changes

--- src/landing/Login.jsx
+++ src/landing/Login.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // CSS
 import './styles/Login.css'
 
-// Images
+// Images and Logo
 import Logo from '../assets/logo-dark.png'
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
 
 export const Login = () => {
+
+    // Email and Password Validation
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+
+    const handleValidation = () => {
+        setEmailError('');
+        setPasswordError('');
+
+        if (!email) {
+            setEmailError('Please enter your email address');
+        } else if (!/\S+@\S+\.\S+/.test(email)) { // Basic email validation
+            setEmailError('Please enter a valid email address');
+        }
+
+        if (!password) {
+            setPasswordError('Please input your password');
+        } else if (password.length < 8) {
+            setPasswordError('Password must be at least 8 characters long');
+        } else if (!/[A-Z]/.test(password)) {
+            setPasswordError('Password must contain at least one uppercase letter');
+        } else if (!/[a-z]/.test(password)) {
+            setPasswordError('Password must contain at least one lowercase letter');
+        } else if (!/[0-9]/.test(password)) {
+            setPasswordError('Password must contain at least one number');
+        } else if (!/[^\w\s]/.test(password)) {
+            setPasswordError('Password must contain at least one special character');
+        }
+    };
+
+    // Show Password
+    const [showPassword, setShowPassword] = useState(false);
+    const [iconType, setIconType] = useState(FaEye); //serves as the Initial Icon
 
     return (
         <>
@@ -20,15 +56,36 @@ export const Login = () => {
                 </div>
                 <div className='inputs'>
                     <div className='input'>
-                        <input type='email' placeholder='Email Address' />
+                        <input
+                            type='email'
+                            placeholder='Email Address'
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </div>
+                    {emailError && <div className='error-message'>{emailError} </div>}
                     <div className='input'>
-                        <input type='password' placeholder='Password' />
+                        <input
+                            type={showPassword ? 'text' : 'password'}
+                            placeholder='Password'
+                            onChange={(e) => {
+                                setPassword(e.target.value)
+                            }}
+                        />
+                        <button
+                            className='show-password-button'
+                            onClick={() => {
+                                setShowPassword(!showPassword);
+                                setIconType(showPassword ? FaEyeSlash : FaEye); // This will Toggle the Icon
+                            }}
+                        >
+                            <span className='password-icon'>{iconType}</span>
+                        </button>
                     </div>
+                    {passwordError && <div className="error-message">{passwordError}</div>}
                 </div>
                 <div className='forgot-password'>Forgot Password? <span>Click Here!</span> </div>
                 <div className='submit-container'>
-                    <div className='submit'>Log In</div>
+                    <button className='submit' onClick={handleValidation}>Log In</button>
                 </div>
             </div>
             <div className='footer'>
