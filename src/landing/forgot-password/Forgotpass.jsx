384861cf91185ade1c453ce2ed236151c5b9ba4f
Roonaaan <rjohnronan2001@gmail.com>
2024-02-14 21:09:53 +0800

    Forgot Password - Bugs Fixed

--- src/landing/forgot-password/Forgotpass.jsx
+++ src/landing/forgot-password/Forgotpass.jsx
@@ -7,44 +7,45 @@ import './styles/Forgotpass.css'
 // Logo
 import Logo from '../../assets/logo-dark.png'
 
-// Navigation
-const navigate = useNavigate();
-const handleLoginClick = () => {
-  navigate("/Login");
-};
 
 export const Forgotpass = () => {
 
-    return(
+    // Navigation
+    const navigate = useNavigate();
+    const handleLoginClick = () => {
+        navigate('/Login');
+    };
+
+    return (
         <>
-        <div className="imageHeader">
-            <img src={Logo} alt='Logo' className='imageHeaderLogo' />
-        </div>
-        <div className="headerContainer">
-            <div className="forgotPassHeader">
-                <div className="forgotPassHeaderTitle"> Reset your Password </div>
+            <div className="imageHeader">
+                <img src={Logo} alt='Logo' className='imageHeaderLogo' />
             </div>
-            <div className="forgotPassHeaderText"> Enter your email address and we will send you instructions to reset your password </div>
-            <div className="inputs">
-                <div className="input">
-                    <input
-                        type='email'
-                        placeholder=''
-                    />
-                    <label for='email'> Email Address </label>
+            <div className="headerContainer">
+                <div className="forgotPassHeader">
+                    <div className="forgotPassHeaderTitle"> Reset your Password </div>
+                </div>
+                <div className="forgotPassHeaderText"> Enter your email address and we will send you instructions to reset your password </div>
+                <div className="inputs">
+                    <div className="input">
+                        <input
+                            type='email'
+                            placeholder=''
+                        />
+                        <label for='email'> Email Address </label>
+                    </div>
+                </div>
+                <div className="forgotSubmit-container">
+                    <button
+                        className='forgotSubmit'
+                    > Continue
+                    </button>
+                </div>
+                <div className="return">
+                    <button className="return-submit" onClick={handleLoginClick}> Click here to return to Login </button>
                 </div>
             </div>
-            <div className="forgotSubmit-container">
-                <button
-                    className='forgotSubmit'
-                > Continue 
-                </button>
-            </div>
-            <div className="return">
-                <button className="return-submit" onClick={handleLoginClick}> Click here to return to Login </button>
-            </div>
-        </div>
-        <div className='footer'>
+            <div className='footer'>
                 <a href=''> Terms of use </a>
                 |
                 <a href=''> Privacy Policy </a>
