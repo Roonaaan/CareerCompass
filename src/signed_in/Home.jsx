83656eb513e0f5235e6776797521ca28756d9c2d
iCodeAzure <bulawan.christoper.bscs2020@gmail.com>
2024-02-26 20:49:21 +0800

    user logged-in - landing page

--- src/signed_in/Home.jsx
+++ src/signed_in/Home.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
 
+import "./styles/styles.css"
+
+import logo from "../assets/final-topright-logo.png";
+
 const Home = () => {
   return (
     <>
-        <h1> Hello world </h1>
+    <div className='parent'>
+    <header className='navBar'>
+      <div className='navBarInner'>
+        <div className='navLogoContainer'>
+          <img src={logo} alt="logo" className="navLogo"/>
+        </div>
+
+        <div className='navProfile'>
+          <input className='userImage' type="image"/>
+          <p className='username'>Welcome, User</p>
+        </div>
+      </div>
+    </header> 
+
+    <section className='createRoadmap'>
+      <div className='headerText'>
+       <p className='welcomeText'> Welcome to CareerCompass </p>
+       <p className='clickText'> Click to create your own roadmap! </p>
+      </div>
+
+      <div className='buttonContainer'>
+      <button className='createButton'> CREATE ROADMAP </button>
+      </div>
+    </section>
+    </div>
+
     </>
   )
 }
