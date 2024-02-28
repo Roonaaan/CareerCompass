2709d74e91217e439181a6b4331d53583a8046ef
Roonaaan <rjohnronan2001@gmail.com>
2024-02-28 17:32:10 +0800

    Updated Database - revised tblaccount

--- ccdb.sql
+++ ccdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 25, 2024 at 06:07 PM
+-- Generation Time: Feb 28, 2024 at 10:30 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,8 +29,9 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `tblaccount` (
   `ID` int(11) NOT NULL,
-  `ACCOUNT_NAME` varchar(255) NOT NULL,
-  `ACCOUNT_USERNAME` varchar(255) NOT NULL,
+  `FIRSTNAME` varchar(255) NOT NULL,
+  `LASTNAME` varchar(255) NOT NULL,
+  `ACCOUNT_EMAIL` varchar(255) NOT NULL,
   `ACCOUNT_PASSWORD` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -38,8 +39,9 @@ CREATE TABLE `tblaccount` (
 -- Dumping data for table `tblaccount`
 --
 
-INSERT INTO `tblaccount` (`ID`, `ACCOUNT_NAME`, `ACCOUNT_USERNAME`, `ACCOUNT_PASSWORD`) VALUES
-(1, 'John Ronan Ramos', 'rjohnronan2001@gmail.com', 'test');
+INSERT INTO `tblaccount` (`ID`, `FIRSTNAME`, `LASTNAME`, `ACCOUNT_EMAIL`, `ACCOUNT_PASSWORD`) VALUES
+(1, 'John Ronan', 'Ramos', 'rjohnronan2001@gmail.com', 'test'),
+(2, 'Adrian', 'Mabuti', 'adrianbarbechomabuti@gmail.com', 'test');
 
 -- --------------------------------------------------------
 
@@ -61,7 +63,8 @@ CREATE TABLE `tblcontactus` (
 INSERT INTO `tblcontactus` (`ID`, `NAME`, `EMAIL`, `MESSAGE`) VALUES
 (1, 'test', 'rjohnronan2001@gmail.com', '0'),
 (2, 'Ronan', 'rjohnronan2001@gmail.com', 'testing '),
-(3, 'AJ', 'jhaykhuletz22@gmail.com', 'test test');
+(3, 'AJ', 'jhaykhuletz22@gmail.com', 'test test'),
+(4, 'test', 'rjohnronan2001@gmail.com', 'test');
 
 -- --------------------------------------------------------
 
@@ -95,7 +98,9 @@ INSERT INTO `tblforgotpass` (`ID`, `EMAIL`, `RESET_TOKEN`, `RESET_TOKEN_EXPIRY`)
 (12, 'rjohnronan2001@gmail.com', '4a4705f17378016085d6d866029236df9acb8f99af8429f7a9080c1c7db6f567', '0000-00-00 00:00:00'),
 (13, 'rjohnronan2001@gmail.com', '4a4705f17378016085d6d866029236df9acb8f99af8429f7a9080c1c7db6f567', '0000-00-00 00:00:00'),
 (14, 'jhaykhuletz22@gmail.com', 'b88a6dbc84b2a110850e03d099e3494a2a79ad444a361e1b32545640ddbafe13', '0000-00-00 00:00:00'),
-(15, 'rjohnronan2001@gmail.com', 'bd8fdc487993ef770c3378d0a5aecf6ee889c2a3627f98a6eab6e2413a654368', '0000-00-00 00:00:00');
+(15, 'rjohnronan2001@gmail.com', 'bd8fdc487993ef770c3378d0a5aecf6ee889c2a3627f98a6eab6e2413a654368', '0000-00-00 00:00:00'),
+(16, 'rjohnronan2001@gmail.com', '7afb037d8e25f854d4e2b6de25285fff694e687d75910e0e9ab8d387a02cdf35', '0000-00-00 00:00:00'),
+(17, 'adrianbarbechomabuti@gmail.com', '1c4eebd1fb73d02ff2c6f598f69fa92c1c9af27874fee3436b95c065095273ab', '0000-00-00 00:00:00');
 
 --
 -- Indexes for dumped tables
@@ -127,19 +132,19 @@ ALTER TABLE `tblforgotpass`
 -- AUTO_INCREMENT for table `tblaccount`
 --
 ALTER TABLE `tblaccount`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tblcontactus`
 --
 ALTER TABLE `tblcontactus`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tblforgotpass`
 --
 ALTER TABLE `tblforgotpass`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
