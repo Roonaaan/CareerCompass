8d9d44ee766fb2192c8c4ce28a3b195d110657df
Roonaaan <rjohnronan2001@gmail.com>
2024-02-29 00:22:40 +0800

    User Landing Page - Added Changes

--- src/signed_in/Home.jsx
+++ src/signed_in/Home.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from 'react'
-
+import { useNavigate } from 'react-router-dom';
 import "./styles/styles.css"
 
 // Images
 import logo from "../assets/final-topright-logo.png";
-import defaultImg from "../assets/signed-in/defaultImg.png"
+import defaultImg from "../assets/signed-in/defaultImg.jpg"
 
 const Home = () => {
 
-  const [userName, setUserName] = useState('');
+  const [userName, setUserName] = useState(''); 
+  const navigate = useNavigate();
 
   // React to PHP Connection
   useEffect(() => {
@@ -34,36 +35,37 @@ const Home = () => {
     }, []);
 
     // Logout User (disabled)
-    /* const handleLogout = () => {
-      still working on this one △do not touch
-    }*/
+    const handleLogout = () => {
+      sessionStorage.removeItem('user');
+      navigate('/');
+    }
 
   return (
     <>
     <div className='parent'>
-    <header className='navBar'>
-      <div className='navBarInner'>
-        <div className='navLogoContainer'>
-          <img src={logo} alt="logo" className="navLogo"/>
+      <header className='navBar'>
+        <div className='navBarInner'>
+          <div className='navLogoContainer'>
+            <img src={logo} alt="logo" className="navLogo"/>
+          </div>
+
+          <div className='navProfile'>
+            <img src={defaultImg} alt='profile' className='profileImg'/>
+            <p className='username'>Welcome, {userName}</p>
+          </div>
+        </div>
+      </header> 
+
+      <section className='createRoadmap'>
+        <div className='headerText'>
+          <p className='welcomeText'> Welcome to CareerCompass </p>
+          <p className='clickText'> Click to create your own roadmap! </p>
         </div>
 
-        <div className='navProfile'>
-          <img src={defaultImg} alt='profile' className='profileImg'/>
-          <p className='username'>Welcome, {userName}</p>
+        <div className='buttonContainer'>
+          <button className='createButton'> CREATE ROADMAP </button>
         </div>
-      </div>
-    </header> 
-
-    <section className='createRoadmap'>
-      <div className='headerText'>
-       <p className='welcomeText'> Welcome to CareerCompass </p>
-       <p className='clickText'> Click to create your own roadmap! </p>
-      </div>
-
-      <div className='buttonContainer'>
-      <button className='createButton'> CREATE ROADMAP </button>
-      </div>
-    </section>
+      </section>
     </div>
 
     </>
