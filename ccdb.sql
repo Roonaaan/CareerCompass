32c15b167e07d475c7621e6ef3424f186d2908c2
Roonaaan <rjohnronan2001@gmail.com>
2024-03-02 21:34:15 +0800

    Database - Added tblprofile

--- ccdb.sql
+++ ccdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 28, 2024 at 10:30 AM
+-- Generation Time: Mar 02, 2024 at 02:33 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,8 +40,9 @@ CREATE TABLE `tblaccount` (
 --
 
 INSERT INTO `tblaccount` (`ID`, `FIRSTNAME`, `LASTNAME`, `ACCOUNT_EMAIL`, `ACCOUNT_PASSWORD`) VALUES
-(1, 'John Ronan', 'Ramos', 'rjohnronan2001@gmail.com', 'test'),
-(2, 'Adrian', 'Mabuti', 'adrianbarbechomabuti@gmail.com', 'test');
+(1, 'John Ronan', 'Ramos', 'rjohnronan2001@gmail.com', 'Test123!'),
+(2, 'Adrian', 'Mabuti', 'adrianbarbechomabuti@gmail.com', 'test'),
+(3, 'Lucky', 'Dancel', 'bscs.dancellp@gmail.com', 'test');
 
 -- --------------------------------------------------------
 
@@ -79,28 +80,29 @@ CREATE TABLE `tblforgotpass` (
   `RESET_TOKEN_EXPIRY` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `tblforgotpass`
+-- Table structure for table `tblprofile`
 --
 
-INSERT INTO `tblforgotpass` (`ID`, `EMAIL`, `RESET_TOKEN`, `RESET_TOKEN_EXPIRY`) VALUES
-(1, 'rjohnronan2001@gmail.com', '4a4705f17378016085d6d866029236df9acb8f99af8429f7a9080c1c7db6f567', '0000-00-00 00:00:00'),
-(2, 'rjohnronan2001@gmail.com', '4a4705f17378016085d6d866029236df9acb8f99af8429f7a9080c1c7db6f567', '0000-00-00 00:00:00'),
-(3, 'rjohnronan2001@gmail.com', '4a4705f17378016085d6d866029236df9acb8f99af8429f7a9080c1c7db6f567', '0000-00-00 00:00:00'),
-(4, 'rjohnronan2001@gmail.com', '4a4705f17378016085d6d866029236df9acb8f99af8429f7a9080c1c7db6f567', '0000-00-00 00:00:00'),
-(5, 'rjohnronan2001@gmail.com', '4a4705f17378016085d6d866029236df9acb8f99af8429f7a9080c1c7db6f567', '0000-00-00 00:00:00'),
-(6, 'rjohnronan2001@gmail.com', '4a4705f17378016085d6d866029236df9acb8f99af8429f7a9080c1c7db6f567', '0000-00-00 00:00:00'),
-(7, 'rjohnronan2001@gmail.com', '4a4705f17378016085d6d866029236df9acb8f99af8429f7a9080c1c7db6f567', '0000-00-00 00:00:00'),
-(8, 'rjohnronan2001@gmail.com', '4a4705f17378016085d6d866029236df9acb8f99af8429f7a9080c1c7db6f567', '0000-00-00 00:00:00'),
-(9, 'rjohnronan2001@gmail.com', '4a4705f17378016085d6d866029236df9acb8f99af8429f7a9080c1c7db6f567', '0000-00-00 00:00:00'),
-(10, 'rjohnronan2001@gmail.com', '4a4705f17378016085d6d866029236df9acb8f99af8429f7a9080c1c7db6f567', '0000-00-00 00:00:00'),
-(11, 'rjohnronan2001@gmail.com', '4a4705f17378016085d6d866029236df9acb8f99af8429f7a9080c1c7db6f567', '0000-00-00 00:00:00'),
-(12, 'rjohnronan2001@gmail.com', '4a4705f17378016085d6d866029236df9acb8f99af8429f7a9080c1c7db6f567', '0000-00-00 00:00:00'),
-(13, 'rjohnronan2001@gmail.com', '4a4705f17378016085d6d866029236df9acb8f99af8429f7a9080c1c7db6f567', '0000-00-00 00:00:00'),
-(14, 'jhaykhuletz22@gmail.com', 'b88a6dbc84b2a110850e03d099e3494a2a79ad444a361e1b32545640ddbafe13', '0000-00-00 00:00:00'),
-(15, 'rjohnronan2001@gmail.com', 'bd8fdc487993ef770c3378d0a5aecf6ee889c2a3627f98a6eab6e2413a654368', '0000-00-00 00:00:00'),
-(16, 'rjohnronan2001@gmail.com', '7afb037d8e25f854d4e2b6de25285fff694e687d75910e0e9ab8d387a02cdf35', '0000-00-00 00:00:00'),
-(17, 'adrianbarbechomabuti@gmail.com', '1c4eebd1fb73d02ff2c6f598f69fa92c1c9af27874fee3436b95c065095273ab', '0000-00-00 00:00:00');
+CREATE TABLE `tblprofile` (
+  `ID` int(11) NOT NULL,
+  `FIRST NAME` varchar(255) NOT NULL,
+  `LAST NAME` varchar(255) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `PHONE_NUMBER` int(20) NOT NULL,
+  `HOME_ADDRESS` varchar(255) NOT NULL,
+  `DISTRICT` int(20) NOT NULL,
+  `CITY` varchar(100) NOT NULL,
+  `PROVINCE` varchar(100) NOT NULL,
+  `POSTAL_CODE` int(10) NOT NULL,
+  `GENDER` varchar(100) NOT NULL,
+  `BIRTHDAY` date NOT NULL,
+  `NATIONALITY` varchar(255) NOT NULL,
+  `CIVIL STATUS` varchar(255) NOT NULL,
+  `JOB_POSITION` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -125,6 +127,12 @@ ALTER TABLE `tblforgotpass`
   ADD PRIMARY KEY (`ID`);
 
 --
+-- Indexes for table `tblprofile`
+--
+ALTER TABLE `tblprofile`
+  ADD PRIMARY KEY (`ID`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -132,7 +140,7 @@ ALTER TABLE `tblforgotpass`
 -- AUTO_INCREMENT for table `tblaccount`
 --
 ALTER TABLE `tblaccount`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tblcontactus`
@@ -144,7 +152,13 @@ ALTER TABLE `tblcontactus`
 -- AUTO_INCREMENT for table `tblforgotpass`
 --
 ALTER TABLE `tblforgotpass`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+
+--
+-- AUTO_INCREMENT for table `tblprofile`
+--
+ALTER TABLE `tblprofile`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
