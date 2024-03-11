917f9113ec5dc5bd895626586eeae5125be1e690
Roonaaan <rjohnronan2001@gmail.com>
2024-03-11 11:08:22 +0800

    User Profile - Modified

--- src/signed_in/Profile.jsx
+++ src/signed_in/Profile.jsx
@@ -122,25 +122,6 @@ const Profile = () => {
               <div className="profileEmpDetail">
                 <p><FontAwesomeIcon icon={faPhone} /> Phone Number </p> {/* PHONE_NUMBER */}
               </div>
-
-              <div className="profileEmpBackgroundEmploymentHistoryContainer">
-                <label htmlFor="empHistory"> Educational Background </label>
-                <div className="profileEmpBackgroundEmploymentHistory">
-                  <div className="profileEmpHistory">
-                    <p> Grade/Level </p> {/* COMPANY */}
-                    <p1> Grade/Level Here </p1>
-                  </div>
-                  <div className="profileEmpHistory">
-                    <p> School: </p> {/* JOB_TITLE */}
-                    <p1> School Here </p1>
-                  </div>
-                  <div className="profileEmpHistory">
-                    <p> Academic Year: </p> {/* COMPANY_ADDRESS */}
-                    <p1> Year Here </p1>
-                  </div>
-                  
-                </div>             
-              </div>
             </div>
           </div>
           {/* Employee Background */}
@@ -222,6 +203,24 @@ const Profile = () => {
                 </div>
               </div>
             </div>
+            <div className="profileEmpBackgroundEmploymentHistoryContainer">
+                <label htmlFor="empHistory"> Educational Background </label>
+                <div className="profileEmpBackgroundEmploymentHistory">
+                  <div className="profileEmpHistory">
+                    <p> Grade/Level </p> {/* COMPANY */}
+                    <p1> Grade/Level Here </p1>
+                  </div>
+                  <div className="profileEmpHistory">
+                    <p> School: </p> {/* JOB_TITLE */}
+                    <p1> School Here </p1>
+                  </div>
+                  <div className="profileEmpHistory">
+                    <p> Academic Year: </p> {/* COMPANY_ADDRESS */}
+                    <p1> Year Here </p1>
+                  </div>
+                  
+                </div>             
+              </div>
           </div>
         </div>
         {showDropdown && <DropdownModal logoutHandler={handleLogout} />}
