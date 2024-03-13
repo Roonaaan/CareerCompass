6d027e23f457b276ba80fcb1e5badf94e7570862
iCodeAzure <bulawan.christoper.bscs2020@gmail.com>
2024-03-13 21:44:52 +0800

    roadmap

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,19 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // Landing, About Us and Login Page
-import Home from './landing/Home';
-import About from './landing/About';
-import ContactUs from './landing/Contact';
+import Home from './landing/Home'
+import About from './landing/About'
+import ContactUs from './landing/Contact'
 
 // Signed-In user page
-import ForgotPasswordChange from './landing/forgot-password/Forgotpasschange';
+import ForgotPasswordChange from './landing/forgot-password/Forgotpasschange'
 
 // Signed-In user page
-import Welcome from './signed_in/Home';
+import Welcome from './signed_in/Home'
 import EmpProfile from './signed_in/Profile'
 import Recommend from './signed_in/Recommend/recommend'
 import SelectDept from './signed_in/Recommend/Selected'
+import Roadmap from './signed_in/Roadmap/Roadmap'
 
 function App() {
 
@@ -33,7 +34,8 @@ function App() {
           <Route path="/Welcome" element={<Welcome />} />
           <Route path="/My-Profile" element={<EmpProfile />} />
           <Route path="/Recommend" element={<Recommend />} />
-          <Route path="/Select-Department" element={<SelectDept />} />         
+          <Route path="/Select-Department" element={<SelectDept />} />    
+          <Route path="/Roadmap" element={<Roadmap />} />       
         </Routes>
       </BrowserRouter>
     </>
---
+++ src/signed_in/Roadmap/Roadmap.jsx
@@ -0,0 +1,137 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+// ROADMAP CSS
+import "./styles/style.css";
+
+import logo from "../../assets/homepage/final-topright-logo.png";
+import defaultImg from "../../assets/signed-in/defaultImg.jpg";
+
+const Roadmap = () => {
+  const [userImage, setUserImage] = useState("");
+  const [userName, setUserName] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const navigate = useNavigate();
+
+  // React to PHP Connection
+  useEffect(() => {
+    const fetchUserName = async () => {
+      try {
+        const userEmail = sessionStorage.getItem("user"); // Retrieve user email from sessionStorage
+        if (userEmail) {
+          const response = await fetch(
+            `http://localhost/CareerCompass/backend/signed-in/home.php?email=${userEmail}`
+          );
+          const data = await response.json();
+
+          if (data.success) {
+            setUserName(data.userName);
+            setUserImage(data.userImage);
+
+            if (data.userImage) {
+              setUserImage(`data:image/jpeg;base64,${data.userImage}`); // Assuming JPEG format, adjust content type if needed
+            } else {
+              setUserImage(defaultImg);
+            }
+          } else {
+            console.log("Failed to fetch user name");
+          }
+        }
+      } catch (error) {
+        console.error("An error occurred", error);
+      }
+    };
+
+    fetchUserName();
+  }, []);
+
+  const handleProfileClick = () => {
+    navigate("/My-Profile");
+  };
+
+  // Logout User
+  const handleLogout = () => {
+    sessionStorage.removeItem("user");
+    navigate("/");
+  };
+
+  const DropdownModal = ({ logoutHandler }) => {
+    return (
+      <div className="dropdown-modal">
+        <div className="profile-info">
+          <img
+            src={userImage || defaultImg}
+            alt="profile"
+            className="profileImg"
+          />
+          <p className="username">{userName}</p>
+        </div>
+        <ul>
+          <li>
+            <button onClick={handleProfileClick}> My Profile </button>
+          </li>
+          <li>
+            <button onClick={logoutHandler}> Log Out </button>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+
+  const toggleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  return (
+    <div className="roadmapWrapper">
+      {/* Navigation Bar */}
+      <header className="navBar">
+        <div className="navBarInner">
+          <div className="navLogoContainer">
+            <img src={logo} alt="logo" className="navLogo" />
+          </div>
+          <div className="navProfile">
+            <img
+              src={userImage || defaultImg}
+              alt="profile"
+              className="profileImg"
+              onClick={toggleDropdown}
+            />
+          </div>
+        </div>
+      </header>
+
+      {/* Middle Section */}
+      <div className="middleSection">
+        <section className="leftSide">
+          <ul className="progressBarList">
+            <li className="progressBarItem">
+              <span className="phaseCount">1</span>
+              <span className="phaseProgressLabel">Phase 1</span>
+            </li>
+
+            <li className="progressBarItem">
+              <span className="phaseCount">2</span>
+              <span className="phaseProgressLabel">Phase 2</span>
+            </li>
+
+            <li className="progressBarItem">
+              <span className="phaseCount">3</span>
+              <span className="phaseProgressLabel">Phase 3</span>
+            </li>
+
+            <li className="progressBarItem">
+              <span className="phaseCount">4</span>
+              <span className="phaseProgressLabel">Phase 4</span>
+            </li>
+          </ul>
+          <div className="progressDescription"></div>
+        </section>
+
+        <section className="rightSide"></section>
+      </div>
+    </div>
+  );
+};
+
+export default Roadmap;
