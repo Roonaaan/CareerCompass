e3491a194ae9c462ba3864d73b99f2a6504008c2
Roonaaan <rjohnronan2001@gmail.com>
2024-03-13 16:49:52 +0800

    Profile Page - Ipdated Frontend

--- src/signed_in/Profile.jsx
+++ src/signed_in/Profile.jsx
@@ -9,36 +9,80 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons'
 
 const Profile = () => {
-  
+
   const [userName, setUserName] = useState('');
   const [showDropdown, setShowDropdown] = useState(false);
   const navigate = useNavigate();
 
   // User Profile
   const [userImage, setUserImage] = useState('');
-  const [userFullName, setFullName] = useState(''); 
+  const [userFullName, setFullName] = useState('');
   const [jobTitle, setJobTitle] = useState('');
   const [empNumber, setEmpNumber] = useState('');
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
-  
+  const [age, setAge] = useState('');
+  const [birthday, setBirthday] = useState('');
+  const [gender, setGender] = useState('');
+  const [nationality, setNationality] = useState('');
+  const [civilStatus, setCivilStatus] = useState('');
+  const [homeAddress, setHomeAddress] = useState('');
+  const [district, setDistrict] = useState('');
+  const [city, setCity] = useState('');
+  const [province, setProvince] = useState('');
+  const [postalCode, setPostalCode] = useState('');
+  const [company, setCompany] = useState('');
+  const [title, setTitle] = useState('');
+  const [companyAddress, setCompanyAddress] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [gradeLevel, setGradeLevel] = useState('');
+  const [school, setSchool] = useState('');
+  const [year, setYear] = useState('');
+  const [employmentHistory, setEmploymentHistory] = useState([]);
+  const [educationalBackground, setEducationalBackground] = useState([]);
+
+
   // React to PHP Connection (Profile)
   useEffect(() => {
     const fetchUserProfile = async () => {
-      try{
+      try {
         const userEmail = sessionStorage.getItem('user');
         if (userEmail) {
           const response = await fetch(`http://localhost/CareerCompass/backend/signed-in/profile.php?email=${userEmail}`);
           const data = await response.json();
 
           if (data.success) {
-            const { userImage, userFullName, jobTitle, empNumber, email, phone } = data.profile;
+            const { userFullName, jobTitle, empNumber, email, phone,
+              age, birthday, gender, nationality, civilStatus,
+              homeAddress, district, city, province, postalCode,
+              company, title, companyAddress, startDate, endDate,
+              gradeLevel, school, year, employmentHistory, educationalBackground } = data.profile;
             setUserImage(`data:image/jpeg;base64,${data.userImage}`);
             setFullName(userFullName);
             setJobTitle(jobTitle);
             setEmpNumber(empNumber);
             setEmail(email);
             setPhone(phone);
+            setAge(age);
+            setBirthday(birthday);
+            setGender(gender);
+            setNationality(nationality);
+            setHomeAddress(homeAddress);
+            setDistrict(district);
+            setCity(city);
+            setProvince(province);
+            setPostalCode(postalCode);
+            setCompany(company);
+            setTitle(title);
+            setCompanyAddress(companyAddress);
+            setStartDate(startDate);
+            setEndDate(endDate);
+            setGradeLevel(gradeLevel);
+            setSchool(school);
+            setYear(year);
+            setEmploymentHistory(employmentHistory);
+            setEducationalBackground(educationalBackground);
           } else {
             console.log('Failed to fetch user profile:', data.message);
           }
@@ -50,8 +94,8 @@ const Profile = () => {
 
     fetchUserProfile();
   }, []);
-  
-  
+
+
 
   // React to PHP Connection
   useEffect(() => {
@@ -97,10 +141,10 @@ const Profile = () => {
     return (
       <div className="dropdown-modal">
         <div className="profile-info">
-          <img 
-            src={userImage || defaultImg} 
-            alt='profile' 
-            className='profileImg' 
+          <img
+            src={userImage || defaultImg}
+            alt='profile'
+            className='profileImg'
           />
           <p className='username'>{userName}</p>
         </div>
@@ -139,7 +183,7 @@ const Profile = () => {
           {/* Employee Profile */}
           <div className="profileEmpContainer">
             <div className="profileEmpImage">
-              <img src={defaultImg} alt='profile' /> {/* IMAGE */}
+              <img src={userImage || defaultImg} alt='profile' /> {/* IMAGE */}
             </div>
             <div className="profileEmpNameContainer">
               <div className="profileEmpNameDetails">
@@ -169,23 +213,23 @@ const Profile = () => {
               <div className="profileEmpBackgroundPersonalInfo">
                 <div className="profileEmpPersonalInfo">
                   <p> Age: </p> {/* AGE */}
-                  <p1> Age Here </p1>
+                  <p1> {age} </p1>
                 </div>
                 <div className="profileEmpPersonalInfo">
                   <p> Birthday: </p> {/* BIRTHDAY */}
-                  <p1> Birthday Here </p1>
+                  <p1> {birthday} </p1>
                 </div>
                 <div className="profileEmpPersonalInfo">
                   <p> Gender: </p> {/* GENDER */}
-                  <p1> Gender Here </p1>
+                  <p1> {gender} </p1>
                 </div>
                 <div className="profileEmpPersonalInfo">
                   <p> Nationality: </p> {/* NATIONALITY */}
-                  <p1> Nationality Here </p1>
+                  <p1> {nationality} </p1>
                 </div>
                 <div className="profileEmpPersonalInfo">
                   <p> Civil Status: </p> {/* CIVIL_STATUS */}
-                  <p1> Civil Status Here </p1>
+                  <p1> {civilStatus} </p1>
                 </div>
               </div>
             </div>
@@ -195,23 +239,23 @@ const Profile = () => {
               <div className="profileEmpBackgroundAddress">
                 <div className="profileEmpAddress">
                   <p> Home Address: </p> {/* HOME_ADDRESS */}
-                  <p1> Home Address Here </p1>
+                  <p1> {homeAddress} </p1>
                 </div>
                 <div className="profileEmpAddress">
                   <p> District: </p> {/* DISTRICT */}
-                  <p1> District Here </p1>
+                  <p1> {district} </p1>
                 </div>
                 <div className="profileEmpAddress">
                   <p> City: </p> {/* CITY */}
-                  <p1> City Here </p1>
+                  <p1> {city} </p1>
                 </div>
                 <div className="profileEmpAddress">
                   <p> Province: </p> {/* PROVINCE */}
-                  <p1> Province Here </p1>
+                  <p1> {province} </p1>
                 </div>
                 <div className="profileEmpAddress">
                   <p> Postal Code: </p> {/* POSTAL_CODE */}
-                  <p1> Postal Code Here </p1>
+                  <p1> {postalCode} </p1>
                 </div>
               </div>
             </div>
@@ -221,44 +265,44 @@ const Profile = () => {
               <div className="profileEmpBackgroundEmploymentHistory">
                 <div className="profileEmpHistory">
                   <p> Company: </p> {/* COMPANY */}
-                  <p1> Company Here </p1>
+                  <p1> {company} </p1>
                 </div>
                 <div className="profileEmpHistory">
                   <p> Job Title: </p> {/* JOB_TITLE */}
-                  <p1> Job Title Here </p1>
+                  <p1> {title} </p1>
                 </div>
                 <div className="profileEmpHistory">
                   <p> Address: </p> {/* COMPANY_ADDRESS */}
-                  <p1> Company Address Here </p1>
+                  <p1> {companyAddress} </p1>
                 </div>
                 <div className="profileEmpHistory">
                   <p> Start Date: </p> {/* START_DATE */}
-                  <p1> Start Date Here </p1>
+                  <p1> {startDate} </p1>
                 </div>
                 <div className="profileEmpHistory">
                   <p> End Date: </p> {/* END_DATE */}
-                  <p1> End Date Here </p1>
+                  <p1> {endDate} </p1>
                 </div>
               </div>
             </div>
+            {/* Educational Background y */}
             <div className="profileEmpBackgroundEmploymentHistoryContainer">
-                <label htmlFor="empHistory"> Educational Background </label>
-                <div className="profileEmpBackgroundEmploymentHistory">
-                  <div className="profileEmpHistory">
-                    <p> Grade/Level </p> {/* COMPANY */}
-                    <p1> Grade/Level Here </p1>
-                  </div>
-                  <div className="profileEmpHistory">
-                    <p> School: </p> {/* JOB_TITLE */}
-                    <p1> School Here </p1>
-                  </div>
-                  <div className="profileEmpHistory">
-                    <p> Academic Year: </p> {/* COMPANY_ADDRESS */}
-                    <p1> Year Here </p1>
-                  </div>
-                  
-                </div>             
+              <label htmlFor="empHistory"> Educational Background </label>
+              <div className="profileEmpBackgroundEmploymentHistory">
+                <div className="profileEmpHistory">
+                  <p> Grade/Level </p> {/* GRADE_LEVEL */}
+                  <p1> {gradeLevel} </p1>
+                </div>
+                <div className="profileEmpHistory">
+                  <p> School: </p> {/* SCHOOL */}
+                  <p1> {school} </p1>
+                </div>
+                <div className="profileEmpHistory">
+                  <p> Year Graduated: </p> {/* YEAR_GRADUATE */}
+                  <p1> {year} </p1>
+                </div>
               </div>
+            </div>
           </div>
         </div>
         {showDropdown && <DropdownModal logoutHandler={handleLogout} />}
