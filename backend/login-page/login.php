b7b8e51e5bee020849a83a61500b142ab16ca00e
Roonaaan <rjohnronan2001@gmail.com>
2024-03-14 16:08:43 +0800

    Errors Fix

--- backend/login-page/login.php
+++ backend/login-page/login.php
@@ -1,6 +1,6 @@
 <?php
 
-    header("Access-Control-Allow-Origin: https://careercmps.netlify.app/");
+    header("Access-Control-Allow-Origin: https://localhost:5173/");
     header("Access-Control-Allow-Methods: GET, POST");
     header("Access-Control-Allow-Headers: Content-Type");
 
